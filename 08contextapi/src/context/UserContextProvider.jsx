ca9d973848fe8d82c0e21a098fb4537e30c034ba
KaachiMitti <samarththakuri98@gmail.com>
2026-01-28 20:44:05 +0530

    context api

--- 08contextapi/src/context/UserContextProvider.jsx
+++ 08contextapi/src/context/UserContextProvider.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import UserContext from "./UserContext.js";
+// children enable using the things
 const UserContextProvider = ({ children }) => {
   const [user, setUser] = React.useState(null);
   return (
